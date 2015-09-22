e952d14fff35bd189a2c0c5cd3b03b8523e88996
smashstack-kris <kris@smashstack.com>
2015-09-22 12:17:24 -0700

    Initial HTML commit

---
+++ dev-index.php
@@ -0,0 +1,26 @@
+<?php include 'assets/templates/project-name/modx/chunks/head.php'; ?>
+
+    </head>
+    
+    <body>
+    
+<?php include 'assets/templates/project-name/modx/chunks/browser_warning.php'; ?>
+
+<?php include 'assets/templates/project-name/modx/chunks/header.php'; ?>
+
+	<div role="main" class="container">
+
+        <!-- Add your site or application content here -->
+        <p>Hello world!</p>
+        
+	</div><!-- .container -->
+	
+<?php include 'assets/templates/project-name/modx/chunks/footer.php'; ?>
+
+<?php include 'assets/templates/project-name/modx/chunks/footer_js.php'; ?>
+
+<?php include 'assets/templates/project-name/modx/chunks/analytics.php'; ?>
+        
+    </body>
+    
+</html>
